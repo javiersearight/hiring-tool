52bc2b65307ff30365b35fbd877507cd2321df51
javiersearight <javier.searight@gmail.com>
2024-10-28 12:40:32 -0500

    added candidates screen with top app bar

--- app/src/main/java/com/javy/hiring/ui/navigation/HiringToolNavHost.kt
+++ app/src/main/java/com/javy/hiring/ui/navigation/HiringToolNavHost.kt
@@ -1,16 +1,10 @@
 package com.javy.hiring.ui.navigation
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.javy.hiring.ui.navigation.Route.CANDIDATES_SCREEN
-import com.javy.hiring.ui.theme.HiringToolTheme
+import com.javy.hiring.ui.screen.candidates.CandidatesScreen
 
 @Composable
 fun HiringToolNavHost() {
@@ -21,29 +15,7 @@ fun HiringToolNavHost() {
         startDestination = CANDIDATES_SCREEN
     ) {
         slideInOutComposable(route = CANDIDATES_SCREEN) {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Greeting(
-                    name = "Android",
-                    modifier = Modifier.padding(innerPadding)
-                )
-            }
+            CandidatesScreen()
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HiringToolTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/javy/hiring/ui/screen/candidates/CandidatesScreen.kt
@@ -0,0 +1,75 @@
+package com.javy.hiring.ui.screen.candidates
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.javy.hiring.R
+import com.javy.hiring.ui.theme.DarkBlue
+import com.javy.hiring.ui.theme.HiringToolTheme
+import com.javy.hiring.ui.theme.White
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CandidatesScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        color = White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .background(DarkBlue)
+            )
+        },
+        contentWindowInsets = WindowInsets(0.dp)
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    CandidatesScreen()
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HiringToolTheme {
+        Greeting("Android")
+    }
+}
